ed83683b479433e5f2f074bbdbaeee6ba572ced0
LiNan <wolf430@163.com>
2023-03-13 13:18:44 +0800

    第一周第一次作业

---
+++ w1-1/README.md
@@ -0,0 +1,20 @@
+
+
+## 账户
+
+0x4BeB80232Ce2Aa5e4304e94b95fD559C4f0383AA
+
+![account](./account.png)
+
+
+
+## 转账记录
+
+哈希：0xab916210df08ac4855a60989fd8c450d0498fa68c010faf1c638f39d93496e74
+
+https://goerli.etherscan.io/tx/0xab916210df08ac4855a60989fd8c450d0498fa68c010faf1c638f39d93496e74
+
+
+
+
+
---
+++ w1-1/counter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Counter{
+    uint public counter;
+
+    constructor(){
+        counter = 0;
+    }
+
+    function count() public{
+        counter = counter + 1;
+    }
+
+    function add(uint x){
+        counter = counter + 2;
+    }
+}
